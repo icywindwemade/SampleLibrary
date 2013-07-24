5c8f55afb82882d64cbbc105d9db91af57c5298a
Rick Cheng <rick.cheng@wemadeusa.com>
2013-07-23 17:53:13 -0700

    Initial Commit

---
+++ SampleLibrary/SampleLibrary.h
@@ -0,0 +1,13 @@
+//
+//  SampleLibrary.h
+//  SampleLibrary
+//
+//  Created by Rickie Cheng on 7/23/13.
+//  Copyright (c) 2013 Rick Cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleLibrary : NSObject
+
+@end
---
+++ SampleLibrary/SampleLibrary.m
@@ -0,0 +1,13 @@
+//
+//  SampleLibrary.m
+//  SampleLibrary
+//
+//  Created by Rickie Cheng on 7/23/13.
+//  Copyright (c) 2013 Rick Cheng. All rights reserved.
+//
+
+#import "SampleLibrary.h"
+
+@implementation SampleLibrary
+
+@end
